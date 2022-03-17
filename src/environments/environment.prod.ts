fa76ba6c29222f8fc22042e6c0ad1a22575ca45e
Ernest Ohene-Asase <kwameoheneasase@gmail.com>
2022-03-17 09:22:56 +0000

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   verifyNumberApi: 'https://apilayer.net/api',
   acces_key:'eb588dbf70cb81df1c8d374269db9d18',
-  countryApi: 'https://restcountries.com/v2/all'
+  countryApi: 'https://restcountries.com/v2/all',
+  supportedCountries: 'https://apilayer.net/api/countries'
 };
